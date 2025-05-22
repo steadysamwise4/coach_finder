79f614d0ecb053f950541c59f3253bad823347f3
steadysamwise4 <davenportsam44@gmail.com>
2025-05-22 12:41:40 -0500

    CoachDetail component

--- src/router.js
+++ src/router.js
@@ -15,6 +15,7 @@ const router = createRouter({
     {
       path: '/coaches/:id',
       component: CoachDetail,
+      props: true,
       children: [
         { path: 'contact', component: CoachContact }, // /coaches/c1/contact
       ],
