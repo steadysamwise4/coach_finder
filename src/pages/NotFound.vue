c37be4c02c962eeb192955499f548217d5c0d5d9
steadysamwise4 <davenportsam44@gmail.com>
2025-06-17 12:27:20 -0500

    animations and route transitions

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1>Page Not Found...</h1>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe check out all of our
+        <router-link to="/coaches">coaches</router-link>.
+      </p></base-card
+    >
+  </section>
 </template>
